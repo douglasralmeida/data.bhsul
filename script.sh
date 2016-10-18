c95862b397ab7a088e6eaef8ad6b59378e7e877e
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-10-18 02:46:05 -0200

    Teste

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-envsubst < "/docker-entrypoint-initdb.d/usuarios.sql" > "/docker-entrypoint-initdb.d/_usuarios.sql"
+envsubst </docker-entrypoint-initdb.d/usuarios.sql>/docker-entrypoint-initdb.d/_usuarios.sql
 rm /docker-entrypoint-initdb.d/usuarios.sql

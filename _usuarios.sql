052678ab14a6bfbb51ca0c02349f8314e7631e6b
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-10-18 01:17:42 -0200

    Testes

--- _usuarios.sql
+++ _usuarios.sql
@@ -1,1 +1,1 @@
-CREATE USER 'usuario_simples'@'%' IDENTIFIED WITH mysql_native_password BY 'PASSWORD_REPLACE';
+CREATE USER 'usuario_simples'@'%' IDENTIFIED WITH mysql_native_password BY 'SQL_PASSWORD';

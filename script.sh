0c226e0dd30996d11013c7d6599803f94a9077df
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-10-18 03:15:49 -0200

    teste

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 envsubst </docker-entrypoint-initdb.d/usuarios.sql>/docker-entrypoint-initdb.d/_usuarios.sql
+tail /docker-entrypoint-initdb.d/_usuarios.sql
 rm /docker-entrypoint-initdb.d/usuarios.sql

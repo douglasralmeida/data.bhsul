e9992c65ef30c817068f1f3f446556fff992e00a
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-10-18 02:31:58 -0200

    Teste

--- script.sh
+++ script.sh
@@ -1,5 +1,2 @@
-export VAR1=$SQL_PASSWORD
-MYVARS='$VAR1'
-
-envsubst "$MYVARS" </docker-entrypoint-initdb.d/usuarios.sql >/docker-entrypoint-initdb.d/_usuarios.sql
+envsubst < "/docker-entrypoint-initdb.d/usuarios.sql" > "/docker-entrypoint-initdb.d/_usuarios.sql"
 rm /docker-entrypoint-initdb.d/usuarios.sql
